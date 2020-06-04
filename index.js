f3971e695e081ed7ce0bfb727dee357b5961f91f
Anthony David Cobb <anthonydcobb@gmail.com>
2020-06-04 20:53:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,7 @@ const input = document.querySelector('input')
 input.addEventListener('keydown', function(event) {
   if (event.key === "g" || console.log(event.key)) {
     return event.preventDefault()
-  } 
+  }
 })
+
+let divs = document.querySelectorAll('div')
