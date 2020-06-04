a6e12dc1f3d9b453eaed62b34e6581ad98e9c017
Anthony David Cobb <anthonydcobb@gmail.com>
2020-06-04 21:00:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ input.addEventListener('keydown', function(event) {
 let divs = document.querySelectorAll('div')
 
 function bubble(e) {
-  console.log(this.firstChild.nodeValue)
+  console.log(this.firstChild)
 }
 
 divs.forEach(function(div) {
